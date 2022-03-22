4ed294ecc4b1512346915aea6d649696994ae68c
Curriculum Team <93026261+CurriculumTeam@users.noreply.github.com>
2022-03-22 13:53:50 +0530

    Add files via upload

---
+++ M6L1A2/app/src/main/java/com/example/m6l1a2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.m6l1a2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
